7fadd35efab912628adf9d6a68a44d32e0bd9cec
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-04-28 16:33:42 +0200

    feat(Typography):added Typography component
    It's intended to help keeping consistency. It still has to handle no-wrap ellipsis

---
+++ src/components/UI/Typography.tsx
@@ -0,0 +1,84 @@
+import {
+  createElement,
+  FunctionComponent
+  // useEffect,
+  // useMemo,
+  // useRef
+} from "react";
+import classes from "./Typography.module.scss";
+
+interface IProps {
+  noWrap?: boolean;
+  variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "h1" | "body" | "p";
+  tag?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "h1" | "div" | "p" | "span";
+  color?:
+    | "error"
+    | "hint"
+    | "info"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "warning";
+}
+
+const Typography: FunctionComponent<IProps> = (props) => {
+  const { children, color, noWrap, tag, variant } = props;
+  const classesList = [];
+  // const childRef = useRef<HTMLElement>(null);
+  // const parentRef = useRef<HTMLDivElement>(null);
+
+  if (variant) classesList.push(classes[variant]);
+  if (color) classesList.push(classes[color]);
+
+  // const resizeObserver = useMemo(
+  //   () =>
+  //     noWrap
+  //       ? new ResizeObserver((entries) => {
+  //           console.log(entries);
+
+  //           if (parentRef.current && childRef.current) {
+  //             childRef.current.style.width = `${parentRef.current?.clientWidth}px`;
+  //           }
+  //         })
+  //       : null,
+  //   [noWrap]
+  // );
+
+  // useEffect(() => {
+  //   const parent = parentRef.current;
+  //   if (parent) resizeObserver?.observe(parent);
+  //   return () => {
+  //     if (parent) resizeObserver?.unobserve(parent);
+  //   };
+  // }, [parentRef, resizeObserver]);
+
+  // if (noWrap) {
+  //   return (
+  //     <div className={classes["no-wrap"]} ref={parentRef}>
+  //       {createElement(tag || "div", {
+  //         children,
+  //         className: classesList.join(" "),
+  //         ref: childRef
+  //       })}
+  //     </div>
+  //   );
+  // }
+
+  if (noWrap) {
+    return (
+      <div className={classes["no-wrap"]}>
+        {createElement(tag ?? variant === "body" ? "div" : variant, {
+          children,
+          className: classesList.join(" ")
+        })}
+      </div>
+    );
+  }
+
+  return createElement(tag ?? variant === "body" ? "div" : variant, {
+    children,
+    className: classesList.join(" ")
+  });
+};
+
+export default Typography;
