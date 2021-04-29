56b30e5f6799026fb9940f830b154d4f007f2f20
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-04-29 05:25:52 +0200

    feat(Avatar-sizes):added sizes to Avatar

--- src/components/UI/Avatar.tsx
+++ src/components/UI/Avatar.tsx
@@ -4,13 +4,20 @@ import { FunctionComponent } from "react";
 interface IProps {
   alt: string;
   src: string;
+  size?: "xs" | "sm" | "md" | "lg" | "xl";
+  outlined?: boolean;
 }
 
 const Avatar: FunctionComponent<IProps> = (props) => {
-  const { alt, children, src } = props;
+  const { alt, children, src, outlined, size } = props;
+
+  const rootClasses = [classes["root"]];
+
+  if (outlined) rootClasses.push(classes["outlined"]);
+  if (size) rootClasses.push(classes[size]);
 
   return (
-    <img alt={alt} src={src} className={classes["root"]}>
+    <img alt={alt} src={src} className={rootClasses.join(" ")}>
       {children}
     </img>
   );
