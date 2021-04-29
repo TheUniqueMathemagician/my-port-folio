11b67806f18a4552ba4cdcfd50ab58f0d74c5193
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-04-29 05:33:01 +0200

    fix(Lock):fixed Lock page with new theming system

--- src/pages/desktop/Lock.tsx
+++ src/pages/desktop/Lock.tsx
@@ -1,8 +1,12 @@
-import { Avatar, Button, Typography } from "@material-ui/core";
 import { useHistory } from "react-router";
 import { useDispatch, useSelector } from "../../hooks/Store";
 import { setCurrentUserID } from "../../store/reducers/Users";
-import styles from "./Lock.module.scss";
+
+import Button from "../../components/UI/Button";
+
+import classes from "./Lock.module.scss";
+import Avatar from "../../components/UI/Avatar";
+import Typography from "../../components/UI/Typography";
 
 export default function Lock() {
   const users = useSelector((store) => store.users.elements);
@@ -10,42 +14,40 @@ export default function Lock() {
   const history = useHistory();
   const dispatch = useDispatch();
   return (
-    <main className={styles["root"]}>
+    <main className={classes["root"]} onMouseDown={() => {}}>
+      <div className={classes["bg"]}></div>
       <section>
         <Avatar
-          className={styles.avatar}
+          size="xl"
           alt="Image de profil"
           src={users[currentUserID].profileImage}
         ></Avatar>
         <Button
-          className={styles.button}
-          variant="outlined"
-          size="large"
+          variant="filled"
+          size="md"
           onClick={() => history.push("/workspace")}
         >
           Se connecter
         </Button>
         <div style={{ position: "fixed", bottom: 0, left: 0, padding: "2rem" }}>
-          {Object.keys(users).map((key) => {
-            return (
-              <Button
-                key={key}
-                variant="outlined"
-                className={styles.button}
-                onClick={() => {
-                  dispatch(setCurrentUserID(key));
-                }}
-              >
-                <Avatar
-                  alt="Image de profil"
-                  src={users[key].profileImage}
-                ></Avatar>
-                <Typography style={{ marginLeft: "1rem" }}>
-                  {users[key].displayName}
-                </Typography>
-              </Button>
-            );
-          })}
+          {Object.keys(users).map((key) => (
+            <Button
+              startIcon
+              variant="blur"
+              size="md"
+              ripple
+              key={key}
+              onClick={() => {
+                dispatch(setCurrentUserID(key));
+              }}
+            >
+              <Avatar
+                alt="Image de profil"
+                src={users[key].profileImage}
+              ></Avatar>
+              <Typography variant="body">{users[key].displayName}</Typography>
+            </Button>
+          ))}
         </div>
       </section>
     </main>
