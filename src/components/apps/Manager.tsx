10f138dc7d1f57410c46192562a9139effe7b664
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-04-29 16:31:31 +0200

    fix(Manager):partially fixed Manager app to fit global refactoring

--- src/components/apps/Manager.tsx
+++ src/components/apps/Manager.tsx
@@ -3,78 +3,21 @@ import { useDispatch, useSelector } from "../../hooks/Store";
 import { closeApplication, sendToFront } from "../../store/reducers/Instances";
 import { DaemonInstance, WindowInstance } from "../../store/reducers/Instances";
 import { MdCenterFocusStrong, MdDelete } from "react-icons/md";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import {
-  Checkbox,
-  Button,
-  ButtonGroup,
-  Tab,
-  Typography,
-  Box,
-  Theme,
-  makeStyles,
-  Tabs
-} from "@material-ui/core";
-
 import { setRunOnStartup } from "../../store/reducers/Applications";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.paper,
-    display: "flex",
-    minHeight: "100%"
-  },
-  tabs: {
-    borderRight: `1px solid ${theme.palette.divider}`
-  },
-  box: {
-    display: "grid",
-    rowGap: "1rem",
-    gridTemplateColumns: "1fr"
-  }
-}));
+import Typography from "../UI/Typography";
+import Paper from "../UI/Paper";
+import TabPanel from "../UI/TabPanel";
+import Tab from "../UI/Tab";
+import Tabs from "../UI/Tabs";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  className?: string;
-  index: any;
-  value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const classes = useStyles();
-  const { className, children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      className={className}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3} className={classes.box}>
-          {children}
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: any) {
-  return {
-    id: `vertical-tab-${index}`,
-    "aria-controls": `vertical-tabpanel-${index}`
-  };
-}
+import classes from "./Manager.module.scss";
+import Table from "../UI/Table";
+import TableHead from "../UI/TableHead";
+import TableRow from "../UI/TableRow";
+import TableCell from "../UI/TableCell";
+import TableBody from "../UI/TableBody";
+import Checkbox from "../UI/Checkbox";
 
 const Manager = () => {
   const applications = useSelector((store) => store.applications);
@@ -92,7 +35,6 @@ const Manager = () => {
       return true;
     }
   );
-  const classes = useStyles();
 
   const [panelIndex, setPanelIndex] = useState(0);
 
@@ -106,127 +48,77 @@ const Manager = () => {
   );
 
   return (
-    <div className={classes.root}>
+    <div className={classes["root"]}>
       <Tabs
-        indicatorColor="primary"
-        orientation="vertical"
-        variant="scrollable"
-        value={panelIndex}
-        onChange={(_, v) => setPanelIndex(v)}
-        aria-label="Vertical tabs example"
-        className={classes.tabs}
+        separator
+        direction="right"
+        defaultValue={0}
+        onChange={(v: number) => setPanelIndex(v)}
       >
-        <Tab label="Applications" {...a11yProps(0)} />
-        <Tab label="Instances" {...a11yProps(1)} />
+        <Tab label="Applications" active={panelIndex === 0} value={0} />
+        <Tab label="Instances" active={panelIndex === 1} value={1} />
       </Tabs>
-      <TabPanel index={0} value={panelIndex}>
-        <Typography variant="h4">Applications</Typography>
-        <Typography variant="body1">Applications installées</Typography>
-        <TableContainer component={Paper} square variant="outlined">
-          <Table aria-label="simple table" size="small">
-            <TableHead style={{ background: "#333" }}>
+      <TabPanel index={0} value={panelIndex} spaced>
+        <Typography variant="h3">Applications</Typography>
+        <Paper outlined spaced>
+          <Typography variant="h4">Applications installées</Typography>
+          <Table aria-label="simple table" size="sm">
+            <TableHead>
               <TableRow>
-                <TableCell style={{ color: "white" }}>Nom</TableCell>
-                <TableCell style={{ color: "white" }} align="center">
+                <TableCell heading>Nom</TableCell>
+                <TableCell heading align="center">
                   AID
                 </TableCell>
-                <TableCell style={{ color: "white" }} align="center">
+                <TableCell heading align="center">
                   Startup
+                </TableCell>
+                <TableCell heading align="center">
+                  Fenêtré
                 </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {Object.keys(applications)
-                .filter((key) => applications[key].type === "window")
-                .map((key) => (
-                  <TableRow key={key}>
-                    <TableCell>{applications[key].displayName}</TableCell>
-                    <TableCell align="center">{applications[key].id}</TableCell>
-                    <TableCell align="center">
-                      <form action="#" onSubmit={(e) => e.preventDefault()}>
-                        <Checkbox
-                          color="primary"
-                          checked={applications[key].runOnStartup}
-                          onChange={(_, value) => {
-                            dispatch(
-                              setRunOnStartup({
-                                application: applications[key],
-                                runOnStartup: value
-                              })
-                            );
-                          }}
-                          inputProps={{ "aria-label": "primary checkbox" }}
-                        ></Checkbox>
-                      </form>
-                    </TableCell>
-                  </TableRow>
-                ))}
+              {Object.keys(applications).map((key) => (
+                <TableRow key={key}>
+                  <TableCell>{applications[key].displayName}</TableCell>
+                  <TableCell align="center">{applications[key].id}</TableCell>
+                  <TableCell align="center">
+                    <form action="#" onSubmit={(e) => e.preventDefault()}>
+                      <Checkbox
+                        color="primary"
+                        checked={applications[key].runOnStartup}
+                        // onChange={(_,) => {
+                        //   dispatch(
+                        //     setRunOnStartup({
+                        //       application: applications[key],
+                        //       runOnStartup: value
+                        //     })
+                        //   );
+                        // }}
+                      ></Checkbox>
+                    </form>
+                  </TableCell>
+                  <TableCell align="center">
+                    <Typography variant="body">
+                      {applications[key].type === "window" ? "Oui" : "Non"}
+                    </Typography>
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
-        </TableContainer>
-        <Typography variant="body1">Services installés</Typography>
-        <TableContainer component={Paper} square variant="outlined">
-          <Table aria-label="simple table" size="small">
-            <TableHead style={{ background: "#333" }}>
-              <TableRow>
-                <TableCell style={{ color: "white" }}>Nom</TableCell>
-                <TableCell style={{ color: "white" }} align="center">
-                  AID
-                </TableCell>
-                <TableCell style={{ color: "white" }} align="center">
-                  Startup
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {Object.keys(applications)
-                .filter((key) => applications[key].type === "daemon")
-                .map((key) => (
-                  <TableRow key={key}>
-                    <TableCell>{applications[key].displayName}</TableCell>
-                    <TableCell align="center">{applications[key].id}</TableCell>
-                    <TableCell align="center">
-                      <form action="#" onSubmit={(e) => e.preventDefault()}>
-                        <Checkbox
-                          checked={applications[key].runOnStartup}
-                          onChange={(_, value) => {
-                            dispatch(
-                              setRunOnStartup({
-                                application: applications[key],
-                                runOnStartup: value
-                              })
-                            );
-                          }}
-                          inputProps={{ "aria-label": "primary checkbox" }}
-                          color="primary"
-                        ></Checkbox>
-                      </form>
-                    </TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        </Paper>
       </TabPanel>
-      <TabPanel index={1} value={panelIndex}>
-        <Typography variant="h4">Instances</Typography>
-        <Typography variant="body1">Instances actives</Typography>
-        <TableContainer
-          component={Paper}
-          square
-          variant="outlined"
-          style={{ backgroundColor: "#ffffffaa" }}
-        >
-          <Table size="small">
-            <TableHead style={{ background: "#333" }}>
+      <TabPanel index={1} value={panelIndex} spaced>
+        <Typography variant="h3">Instances</Typography>
+        <Paper outlined spaced>
+          <Typography variant="h4">Instances actives</Typography>
+          <Table>
+            <TableHead>
               <TableRow>
-                <TableCell style={{ color: "white" }}>Nom</TableCell>
-                <TableCell style={{ color: "white" }} align="center">
-                  PID
-                </TableCell>
-                <TableCell style={{ color: "white" }} align="center">
-                  Actions
-                </TableCell>
+                <TableCell>Nom</TableCell>
+                <TableCell align="center">PID</TableCell>
+                <TableCell align="center">Actions</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -235,7 +127,7 @@ const Manager = () => {
                   <TableCell>{instances[key].displayName}</TableCell>
                   <TableCell align="center">{instances[key].id}</TableCell>
                   <TableCell align="center">
-                    <ButtonGroup disableElevation variant="outlined">
+                    {/* <ButtonGroup disableElevation variant="outlined">
                       <Button
                         size="large"
                         onClick={() => {
@@ -256,13 +148,13 @@ const Manager = () => {
                           <MdCenterFocusStrong color="primary"></MdCenterFocusStrong>
                         </Button>
                       )}
-                    </ButtonGroup>
+                    </ButtonGroup> */}
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
-        </TableContainer>
+        </Paper>
       </TabPanel>
     </div>
   );
