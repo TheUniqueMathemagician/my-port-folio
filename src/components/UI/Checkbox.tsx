687623c5b0d52081ac7f91b217d3e253c55880de
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-02 23:36:45 +0200

    feat(Checkbox):added checkbox implementation

--- src/components/UI/Checkbox.tsx
+++ src/components/UI/Checkbox.tsx
@@ -1,13 +1,16 @@
+import classes from "./Checkbox.module.scss";
 import { FunctionComponent } from "react";
 import { TSize } from "../../types/TSize";
+import Mark from "../icons/Mark";
 
 interface IProps {
+  ariaLabel?: string;
   color?: "primary" | "secondary" | "info" | "success" | "warning" | "error";
   disabled?: boolean;
-  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onClick?: (e: React.MouseEvent<HTMLLabelElement, MouseEvent>) => void;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   outlined?: boolean;
-  ripple?: boolean;
+  name?: string;
   size?: TSize;
   startIcon?: boolean;
   checked: boolean;
@@ -16,8 +19,42 @@ interface IProps {
 
 // "aria-label": "primary checkbox"
 
-const Checkbox: FunctionComponent<IProps> = () => {
-  return <input type="checkbox" name="" onChange={(e) => {}}></input>;
+const Checkbox: FunctionComponent<IProps> = (props) => {
+  const {
+    ariaLabel,
+    checked,
+    color,
+    disabled,
+    name,
+    onChange,
+    onClick,
+    outlined,
+    size,
+    variant
+  } = props;
+  const rootClasses = [classes["root"]];
+
+  if (outlined) rootClasses.push(classes["outlined"]);
+  if (size) rootClasses.push(classes[size]);
+  if (variant) rootClasses.push(classes[variant]);
+  if (color) rootClasses.push(classes[color]);
+
+  return (
+    <label className={rootClasses.join(" ")} onClick={onClick}>
+      <input
+        disabled={disabled}
+        checked={checked}
+        type="checkbox"
+        name={name}
+        onChange={onChange}
+        aria-label={ariaLabel}
+      ></input>
+      <div className={classes["checkbox"]}>
+        <div className={classes["effect"]}></div>
+        <Mark></Mark>
+      </div>
+    </label>
+  );
 };
 
 export default Checkbox;
