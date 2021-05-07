5360b2d3c323ebff40afff36e08580aa2f1f4191
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-07 18:08:33 +0200

    feat(Avatar & TabPanel):added element default options

--- src/components/UI/Avatar.tsx
+++ src/components/UI/Avatar.tsx
@@ -1,30 +1,43 @@
 import classes from "./Avatar.module.scss";
-import { FunctionComponent, memo } from "react";
+import { FunctionComponent, memo, useCallback } from "react";
 import { TSize } from "../../types/TSize";
 
-interface IProps {
+interface IProps
+  extends React.DetailedHTMLProps<
+    React.ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  > {
   alt: string;
   src: string;
   size?: TSize;
   outlined?: boolean;
+  square?: boolean;
 }
 
 const Avatar: FunctionComponent<IProps> = (props) => {
-  const { alt, src, outlined, size } = props;
+  const { alt, className, src, outlined, size, square, ...other } = props;
+
+  const handleDragStart = useCallback(
+    (e: React.DragEvent<HTMLImageElement>) => {
+      e.preventDefault();
+    },
+    []
+  );
 
   const rootClasses = [classes["root"]];
 
   if (outlined) rootClasses.push(classes["outlined"]);
   if (size) rootClasses.push(classes[size]);
+  if (square) rootClasses.push(classes["square"]);
+  if (className) rootClasses.push(className);
 
   return (
     <img
-      onDragStart={(e) => {
-        e.preventDefault();
-      }}
+      onDragStart={handleDragStart}
       alt={alt}
       src={src}
       className={rootClasses.join(" ")}
+      {...other}
     ></img>
   );
 };
--- src/components/UI/TabPanel.tsx
+++ src/components/UI/TabPanel.tsx
@@ -1,37 +1,41 @@
 import classes from "./TabPanel.module.scss";
 import { FunctionComponent } from "react";
 
-interface IPropsA {
-  children?: React.ReactNode;
-  className?: string;
+interface IPropsA
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
   index: number;
   spaced?: boolean;
   value: number;
 }
 
-interface IPropsB {
-  children?: React.ReactNode;
-  className?: string;
+interface IPropsB
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
   index: string;
   spaced?: boolean;
   value: string;
 }
 
 const TabPanel: FunctionComponent<IPropsA | IPropsB> = (props) => {
-  const { className, children, spaced, value, index, ...other } = props;
+  const { children, spaced, value, index, className, ...other } = props;
   const rootClasses = [classes["root"]];
+  if (className) rootClasses.push(className);
   if (spaced) rootClasses.push(classes["spaced"]);
-  return (
+  return value === index ? (
     <div
       role="tabpanel"
-      hidden={value !== index}
       className={rootClasses.join(" ")}
       // aria-labelledby={`vertical-tab-${index}`}
       {...other}
     >
       {value === index && children}
     </div>
-  );
+  ) : null;
 };
 
 export default TabPanel;
