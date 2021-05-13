272089ce52efcdf7f7f6fdd4233e58382a1e1a97
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-13 13:59:07 +0200

    feat(Text):added Text component

--- src/components/UI/Input/Text.tsx
+++ src/components/UI/Input/Text.tsx
@@ -1,16 +1,27 @@
-import classes from "./Text.module.css";
+import classes from "./Text.module.scss";
 import { FunctionComponent } from "react";
 
-export interface IProps {}
+export interface IProps {
+  readonly label?: string;
+  readonly required?: boolean;
+  readonly type?: "password" | "text" | "email" | "tel";
+  readonly validator?: RegExp;
+  readonly fullWidth?: boolean;
+}
 
 const Text: FunctionComponent<IProps> = (props) => {
-  const {} = props;
+  const { fullWidth, label, required, type } = props;
+
   const rootClasses = [classes["root"]];
+
+  if (fullWidth) rootClasses.push(classes["full-width"]);
+
   return (
-    <div className={rootClasses.join(" ")}>
-      <input type="text"></input>
+    <label className={rootClasses.join(" ")}>
+      <input required={required} type={type ?? "text"}></input>
+      {label && <span>{label}</span>}
       <div className={classes["effect"]}></div>
-    </div>
+    </label>
   );
 };
 
