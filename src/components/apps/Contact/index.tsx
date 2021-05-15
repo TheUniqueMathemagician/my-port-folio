9b2ce04f359fabc010a6671ed609b5319999d614
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-15 17:27:34 +0200

    feat(Contact):added antispam

---
+++ src/components/apps/Contact/index.tsx
@@ -0,0 +1,206 @@
+import classes from "./index.module.scss";
+
+import { useDispatch, useSelector } from "../../../hooks/Store";
+import { runApplication } from "../../../store/reducers/Instances";
+import { FunctionComponent, memo, useCallback, useState } from "react";
+
+import { MdMail, MdPhone } from "react-icons/md";
+import { IoLocationSharp } from "react-icons/io5";
+
+import Button from "../../UI/Input/Button";
+import Paper from "../../UI/Paper";
+import Typography from "../../UI/Typography";
+import Text from "../../UI/Input/Text";
+import TextArea from "../../UI/Input/TextArea";
+import Time from "./Elements/Time";
+
+const Sharp = memo(IoLocationSharp);
+const Mail = memo(MdMail);
+const Phone = memo(MdPhone);
+
+interface IProps {}
+
+const Contact: FunctionComponent<IProps> = () => {
+  const dispatch = useDispatch();
+
+  const maps = useSelector(
+    (store) =>
+      store.applications.elements[
+        Object.keys(store.applications.elements).find(
+          (key) => store.applications.elements[key].displayName === "Maps"
+        ) ?? ""
+      ]
+  );
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [resendDate, setResendDate] = useState<Date>(
+    new Date(parseInt(localStorage.getItem("contact-resend-date") ?? "0"))
+  );
+
+  const handleMapsClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      e.preventDefault();
+      if (maps) {
+        dispatch(runApplication({ application: maps, args: {} }));
+      }
+    },
+    [maps, dispatch]
+  );
+
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (loading) return;
+
+      setLoading(true);
+
+      const headers = new Headers();
+      const data = new FormData(e.target as HTMLFormElement);
+      const body = JSON.stringify(Object.fromEntries(data.entries()));
+
+      headers.append("Content-Type", "application/json");
+
+      try {
+        const response = await fetch("/api/contact", {
+          method: "post",
+          headers,
+          body
+        });
+        if (response.status === 200 || response.status === 429) {
+          const text = await response.text();
+          localStorage.setItem("contact-resend-date", text);
+          setResendDate(new Date(parseInt(text)));
+        }
+      } catch (_) {
+      } finally {
+        setLoading(false);
+      }
+    },
+    [loading]
+  );
+
+  return (
+    <div className={classes["root"]}>
+      <Typography variant="h4">Me contacter</Typography>
+      <Paper spaced blur background="paper">
+        <div className={classes["grid"]}>
+          <Typography variant="body">Email</Typography>
+          <Button
+            align="start"
+            size="md"
+            variant="filled"
+            focusable
+            startIcon
+            to="mailto: tamburrini.yannick@gmail.com"
+          >
+            <Mail></Mail>
+            <Typography variant="body" noWrap>
+              tamburrini.yannick@gmail.com
+            </Typography>
+          </Button>
+          <Typography variant="body">Téléphone</Typography>
+          <Button
+            align="start"
+            size="md"
+            focusable
+            variant="filled"
+            startIcon
+            to="tel:+32 498 62 77 16"
+          >
+            <Phone></Phone>
+            <Typography variant="body" noWrap>
+              +32 498 62 77 16
+            </Typography>
+          </Button>
+          <Typography variant="body">Adresse</Typography>
+          <Button
+            align="start"
+            size="md"
+            focusable
+            variant="filled"
+            startIcon
+            onClick={handleMapsClick}
+            to="https://www.google.com/maps/place/Avenue+des+Lanciers+45,+4900+Spa/@50.4888358,5.8542398,17z/data=!3m1!4b1!4m5!3m4!1s0x47c0617fcf8a2513:0xe0e509238ab82a8e!8m2!3d50.4888324!4d5.8564285"
+          >
+            <Sharp></Sharp>
+            <Typography variant="body" noWrap>
+              Avenue des Lanciers 45, 4900 Spa, Liège Belgique
+            </Typography>
+          </Button>
+        </div>
+      </Paper>
+      <Typography variant="h4">M'envoyer un message</Typography>
+      <Paper spaced blur background="paper">
+        <form action="" onSubmit={handleSubmit}>
+          <Text
+            disabled={loading}
+            fullWidth
+            label="Nom*"
+            name="lastname"
+            required
+          ></Text>
+          <Text
+            disabled={loading}
+            fullWidth
+            label="Prénom*"
+            name="firstname"
+            required
+          ></Text>
+          <Text
+            disabled={loading}
+            fullWidth
+            label="Organisation*"
+            name="org"
+            required
+          ></Text>
+          <Text
+            disabled={loading}
+            fullWidth
+            label="Email*"
+            name="email"
+            required
+            type="email"
+          ></Text>
+          <Text
+            disabled={loading}
+            fullWidth
+            label="Téléphone"
+            name="tel"
+            type="tel"
+          ></Text>
+          <TextArea
+            disabled={loading}
+            className={classes["text-area"]}
+            fullWidth
+            label="Message*"
+            name="msg"
+            required
+            vertical
+          ></TextArea>
+          <Button
+            disabled={loading || resendDate.getTime() > 0}
+            ripple
+            focusable
+            color="primary"
+            outlined
+            size="md"
+            loading={loading}
+          >
+            {resendDate.getTime() > 0 ? (
+              <Time
+                date={resendDate}
+                onAvailable={() => setResendDate(new Date(0))}
+              ></Time>
+            ) : (
+              "Envoyer"
+            )}
+          </Button>
+          <Typography variant="body">( * champs requis )</Typography>
+        </form>
+      </Paper>
+    </div>
+  );
+};
+
+export default memo(Contact);
