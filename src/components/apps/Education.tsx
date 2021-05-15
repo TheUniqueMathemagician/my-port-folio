f474d1e7029fcb22bf8bccff5b89def5c3e26223
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-15 22:09:06 +0200

    chore(Education & Experience):removed components

--- src/components/apps/Education.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent } from "react";
-
-interface IProps {}
-
-const Education: FunctionComponent<IProps> = () => {
-  return <div>Education app</div>;
-};
-
-export default Education;
--- src/components/apps/Experience.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionComponent } from "react";
-
-interface IProps {}
-
-const Experience: FunctionComponent<IProps> = () => {
-  return <div>Experience app</div>;
-};
-
-export default Experience;
