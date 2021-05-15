a3bedfde08ec0073485bbbe347370c149577ef32
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-15 18:37:24 +0200

    refacto(Checkbox):extended default checkbox interface

--- src/components/UI/Input/Checkbox.tsx
+++ src/components/UI/Input/Checkbox.tsx
@@ -3,48 +3,30 @@ import { FunctionComponent, memo } from "react";
 import { TSize } from "../../../types/TSize";
 import Mark from "../../icons/Mark";
 
-interface IProps {
+interface IProps
+  extends Omit<
+    React.DetailedHTMLProps<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      HTMLInputElement
+    >,
+    "onClick" | "size" | "type"
+  > {
   color?: "primary" | "secondary" | "info" | "success" | "warning" | "error";
-  disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLLabelElement>;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  outlined?: boolean;
-  name?: string;
   size?: TSize;
-  startIcon?: boolean;
-  checked: boolean;
-  variant?: "flat" | "filled" | "blur";
 }
 
 const Checkbox: FunctionComponent<IProps> = (props) => {
-  const {
-    checked,
-    color,
-    disabled,
-    name,
-    onChange,
-    onClick,
-    outlined,
-    size,
-    variant
-  } = props;
+  const { className, color, onClick, size, ...other } = props;
   const rootClasses = [classes["root"]];
 
-  if (outlined) rootClasses.push(classes["outlined"]);
   if (size) rootClasses.push(classes[size]);
-  if (variant) rootClasses.push(classes[variant]);
   if (color) rootClasses.push(classes[color]);
+  if (className) rootClasses.push(className);
 
   return (
     <label className={rootClasses.join(" ")} onClick={onClick}>
-      <input
-        disabled={disabled}
-        checked={checked}
-        type="checkbox"
-        name={name}
-        onChange={onChange}
-        aria-label="Checkbox"
-      ></input>
+      <input type="checkbox" {...other}></input>
       <div className={classes["checkbox"]}>
         <div className={classes["effect"]}></div>
         <Mark></Mark>
