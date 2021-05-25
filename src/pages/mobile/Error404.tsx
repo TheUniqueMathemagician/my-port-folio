4da299b4f68842335d894db24e8402826af57787
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-25 15:01:35 +0200

    refacto(Error404): added classes

--- src/pages/mobile/Error404.tsx
+++ src/pages/mobile/Error404.tsx
@@ -1,12 +1,12 @@
+import classes from "./Error404.module.scss";
 import { Link } from "react-router-dom";
-import React from "react";
 
 const Error404 = () => {
   return (
-    <>
-      <p>Oops. La page que vous avez demandé n'existe pas.</p>
+    <main className={classes["root"]}>
+      <p>Oops. La page que vous avez demandée n'existe pas.</p>
       <Link to="/">Accueil</Link>
-    </>
+    </main>
   );
 };
 
