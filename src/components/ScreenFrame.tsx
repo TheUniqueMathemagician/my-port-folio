c2481ee832d24747d4892bd88b430b57e297b79a
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-25 15:19:11 +0200

    fix(ScreenFrame):fixed mobile layout

--- src/components/ScreenFrame.tsx
+++ src/components/ScreenFrame.tsx
@@ -1,9 +1,15 @@
-import styles from "./ScreenFrame.module.scss";
+import classes from "./ScreenFrame.module.scss";
 import { useSelector } from "../hooks/Store";
 import { FunctionComponent } from "react";
 
 const ScreenFrame: FunctionComponent = ({ children }) => {
   const bg = useSelector((store) => store.theme.workspaceBackgroundURL);
+
+  const isMobile = useSelector((store) => store.os.isMobile);
+
+  const rootClasses = [classes["root"]];
+
+  if (isMobile) rootClasses.push(classes["mobile"]);
 
   return (
     <div
@@ -13,7 +19,7 @@ const ScreenFrame: FunctionComponent = ({ children }) => {
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover"
       }}
-      className={styles["screen-frame"]}
+      className={rootClasses.join(" ")}
     >
       {children}
     </div>
