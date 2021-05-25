7e0470a4fe39259a6243d88212d2c8a74d5a368d
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-25 14:57:46 +0200

    refacto(Desktop): refactored router

--- src/layouts/Desktop.tsx
+++ src/layouts/Desktop.tsx
@@ -1,10 +1,10 @@
-import Index from "../pages/desktop/Index";
+import Boot from "../pages/desktop/Boot";
 import Error404 from "../pages/desktop/Error404";
-import WorkSpace from "../pages/desktop/WorkSpace";
+import Index from "../pages/desktop/Index";
 import Lock from "../pages/desktop/Lock";
+import WorkSpace from "../pages/desktop/WorkSpace";
 
 import { Switch, Route, useHistory } from "react-router-dom";
-import Boot from "../pages/desktop/Boot";
 import { useLayoutEffect } from "react";
 import { useSelector } from "../hooks/Store";
 
@@ -20,21 +20,11 @@ const Desktop = () => {
 
   return (
     <Switch>
-      <Route exact path="/">
-        <Index></Index>
-      </Route>
-      <Route path="/boot">
-        <Boot></Boot>
-      </Route>
-      <Route path="/lock">
-        <Lock></Lock>
-      </Route>
-      <Route path="/workspace">
-        <WorkSpace></WorkSpace>
-      </Route>
-      <Route path="*">
-        <Error404></Error404>
-      </Route>
+      <Route exact path="/" component={Index}></Route>
+      <Route exact path="/boot" component={Boot}></Route>
+      <Route exact path="/lock" component={Lock}></Route>
+      <Route exact path="/workspace" component={WorkSpace}></Route>
+      <Route exact path="*" component={Error404}></Route>
     </Switch>
   );
 };
