d6e945260ebf42b3d3cf4836965002e42f6000c4
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-05-25 15:32:48 +0200

    fix(Contact):adapted for mobiles

--- src/components/apps/Contact/index.tsx
+++ src/components/apps/Contact/index.tsx
@@ -25,6 +25,7 @@ interface IProps {}
 const Contact: FunctionComponent<IProps> = () => {
   const dispatch = useDispatch();
 
+  const isMobile = useSelector((store) => store.os.isMobile);
   const contrast = useSelector(
     (store) => store.theme.colorScheme === EColorScheme.contrast
   );
@@ -75,129 +76,135 @@ const Contact: FunctionComponent<IProps> = () => {
     [loading]
   );
 
+  const rootClasses = [classes["root"]];
+
+  if (isMobile) rootClasses.push(classes["mobile"]);
+
   return (
-    <div className={classes["root"]}>
-      <Typography variant="h4">Me contacter</Typography>
-      <Paper outlined={contrast} spaced blur background="paper">
-        <div className={classes["grid"]}>
-          <Typography variant="body">Email</Typography>
-          <Button
-            align="start"
-            focusable
-            outlined
-            size="md"
-            startIcon
-            to="mailto: tamburrini.yannick@gmail.com"
-            variant="filled"
-          >
-            <Mail></Mail>
-            <Typography variant="body" noWrap>
-              tamburrini.yannick@gmail.com
-            </Typography>
-          </Button>
-          <Typography variant="body">Téléphone</Typography>
-          <Button
-            align="start"
-            focusable
-            outlined
-            size="md"
-            startIcon
-            to="tel:+32 498 62 77 16"
-            variant="filled"
-          >
-            <Phone></Phone>
-            <Typography variant="body" noWrap>
-              +32 498 62 77 16
-            </Typography>
-          </Button>
-          <Typography variant="body">Adresse</Typography>
-          <Button
-            align="start"
-            focusable
-            onClick={handleMapsClick}
-            outlined
-            size="md"
-            startIcon
-            to="https://www.google.com/maps/place/Avenue+des+Lanciers+45,+4900+Spa/@50.4888358,5.8542398,17z/data=!3m1!4b1!4m5!3m4!1s0x47c0617fcf8a2513:0xe0e509238ab82a8e!8m2!3d50.4888324!4d5.8564285"
-            variant="filled"
-          >
-            <Sharp></Sharp>
-            <Typography variant="body" noWrap>
-              Avenue des Lanciers 45, 4900 Spa, Liège Belgique
-            </Typography>
-          </Button>
-        </div>
-      </Paper>
-      <Typography variant="h4">M'envoyer un message</Typography>
-      <Paper outlined={contrast} spaced blur background="paper">
-        <form action="" onSubmit={handleSubmit}>
-          <Text
-            disabled={loading}
-            fullWidth
-            label="Nom*"
-            name="lastname"
-            required
-          ></Text>
-          <Text
-            disabled={loading}
-            fullWidth
-            label="Prénom*"
-            name="firstname"
-            required
-          ></Text>
-          <Text
-            disabled={loading}
-            fullWidth
-            label="Organisation*"
-            name="org"
-            required
-          ></Text>
-          <Text
-            disabled={loading}
-            fullWidth
-            label="Email*"
-            name="email"
-            required
-            type="email"
-          ></Text>
-          <Text
-            disabled={loading}
-            fullWidth
-            label="Téléphone"
-            name="tel"
-            type="tel"
-          ></Text>
-          <TextArea
-            disabled={loading}
-            className={classes["text-area"]}
-            fullWidth
-            label="Message*"
-            name="msg"
-            required
-            vertical
-          ></TextArea>
-          <Button
-            disabled={loading || resendDate.getTime() > 0}
-            ripple
-            focusable
-            color="primary"
-            outlined
-            size="md"
-            variant="filled"
-            loading={loading}
-          >
-            {resendDate.getTime() > 0 ? (
-              <Time
-                date={resendDate}
-                onAvailable={() => setResendDate(new Date(0))}
-              ></Time>
-            ) : (
-              "Envoyer"
-            )}
-          </Button>
-          <Typography variant="body">( * champs requis )</Typography>
-        </form>
-      </Paper>
+    <div className={rootClasses.join(" ")}>
+      <div className={classes["container"]}>
+        <Typography variant="h4">Me contacter</Typography>
+        <Paper outlined={contrast} spaced blur background="paper">
+          <div className={classes["grid"]}>
+            <Typography variant="body">Email</Typography>
+            <Button
+              align="start"
+              focusable
+              outlined
+              size="md"
+              startIcon
+              to="mailto: tamburrini.yannick@gmail.com"
+              variant="filled"
+            >
+              <Mail></Mail>
+              <Typography variant="body" noWrap>
+                tamburrini.yannick@gmail.com
+              </Typography>
+            </Button>
+            <Typography variant="body">Téléphone</Typography>
+            <Button
+              align="start"
+              focusable
+              outlined
+              size="md"
+              startIcon
+              to="tel:+32 498 62 77 16"
+              variant="filled"
+            >
+              <Phone></Phone>
+              <Typography variant="body" noWrap>
+                +32 498 62 77 16
+              </Typography>
+            </Button>
+            <Typography variant="body">Adresse</Typography>
+            <Button
+              align="start"
+              focusable
+              onClick={handleMapsClick}
+              outlined
+              size="md"
+              startIcon
+              to="https://www.google.com/maps/place/Avenue+des+Lanciers+45,+4900+Spa/@50.4888358,5.8542398,17z/data=!3m1!4b1!4m5!3m4!1s0x47c0617fcf8a2513:0xe0e509238ab82a8e!8m2!3d50.4888324!4d5.8564285"
+              variant="filled"
+            >
+              <Sharp></Sharp>
+              <Typography variant="body" noWrap>
+                Avenue des Lanciers 45, 4900 Spa, Liège Belgique
+              </Typography>
+            </Button>
+          </div>
+        </Paper>
+        <Typography variant="h4">M'envoyer un message</Typography>
+        <Paper outlined={contrast} spaced blur background="paper">
+          <form action="" onSubmit={handleSubmit}>
+            <Text
+              disabled={loading}
+              fullWidth
+              label="Nom*"
+              name="lastname"
+              required
+            ></Text>
+            <Text
+              disabled={loading}
+              fullWidth
+              label="Prénom*"
+              name="firstname"
+              required
+            ></Text>
+            <Text
+              disabled={loading}
+              fullWidth
+              label="Organisation*"
+              name="org"
+              required
+            ></Text>
+            <Text
+              disabled={loading}
+              fullWidth
+              label="Email*"
+              name="email"
+              required
+              type="email"
+            ></Text>
+            <Text
+              disabled={loading}
+              fullWidth
+              label="Téléphone"
+              name="tel"
+              type="tel"
+            ></Text>
+            <TextArea
+              disabled={loading}
+              className={classes["text-area"]}
+              fullWidth
+              label="Message*"
+              name="msg"
+              required
+              vertical
+            ></TextArea>
+            <Button
+              disabled={loading || resendDate.getTime() > 0}
+              ripple
+              focusable
+              color="primary"
+              outlined
+              size="md"
+              variant="filled"
+              loading={loading}
+            >
+              {resendDate.getTime() > 0 ? (
+                <Time
+                  date={resendDate}
+                  onAvailable={() => setResendDate(new Date(0))}
+                ></Time>
+              ) : (
+                "Envoyer"
+              )}
+            </Button>
+            <Typography variant="body">( * champs requis )</Typography>
+          </form>
+        </Paper>
+      </div>
     </div>
   );
 };
