7f7856692d74b799e1012ab6c1040cb0b9ea806c
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-11-07 18:40:43 +0100

    feat(ShortCutFrame): added ShortCutFrame component

---
+++ src/shared/os/ShortcutFrame.tsx
@@ -0,0 +1,14 @@
+import {FC, memo} from "react";
+import {useSelector} from "../../hooks/Store";
+import Shortcut from "./Shortcut";
+import styles from "./ShortcutFrame.module.scss";
+
+const ShortcutFrame: FC = () => {
+  const aids = useSelector((store) => Object.keys(store.applications.pool).filter((key) => store.applications.pool[+key].shortcut).map((key) => +key));
+
+  return <div className={styles["root"]}>
+    {aids.map((aid) => (<Shortcut aid={aid} key={aid}></Shortcut>))}
+  </div>;
+};
+
+export default memo(ShortcutFrame);
