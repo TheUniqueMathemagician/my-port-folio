7de159e7120e7de99831f4ce92dc3d22f63e3ebf
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-11-07 14:12:33 +0100

    feat(Checkbox): added Checkbox component

---
+++ src/shared/ui/input/Checkbox.tsx
@@ -0,0 +1,30 @@
+import {FC, memo} from "react";
+import {TSize} from "../../../types/TSize";
+import Mark from "../../icons/Mark";
+import classes from "./Checkbox.module.scss";
+
+interface Props extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, "onClick" | "size" | "type"> {
+  color?: "primary" | "secondary" | "info" | "success" | "warning" | "error";
+  onClick?: React.MouseEventHandler<HTMLLabelElement>;
+  size?: TSize;
+}
+
+const Checkbox: FC<Props> = (props) => {
+  const {className, color, onClick, size, ...other} = props;
+
+  const rootClasses = [classes["root"]];
+
+  if (size) rootClasses.push(classes[size]);
+  if (color) rootClasses.push(classes[color]);
+  if (className) rootClasses.push(className);
+
+  return <label className={rootClasses.join(" ")} onClick={onClick}>
+    <input type="checkbox" {...other}></input>
+    <div className={classes["checkbox"]}>
+      <div className={classes["effect"]}></div>
+      <Mark></Mark>
+    </div>
+  </label>;
+};
+
+export default memo(Checkbox);
