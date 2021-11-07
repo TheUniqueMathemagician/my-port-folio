71e2b6e1fd1e5c08b22f476d54ce9fcd22736a24
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-11-07 14:10:33 +0100

    feat(Radio): added Radio component

---
+++ src/shared/ui/input/Radio.tsx
@@ -0,0 +1,28 @@
+import {DetailedHTMLProps, FunctionComponent, InputHTMLAttributes, memo, MouseEventHandler} from "react";
+import classes from "./Radio.module.scss";
+
+interface IProps extends Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, "onClick" | "type"> {
+  readonly color?: "primary" | "secondary" | "info" | "success" | "warning" | "error";
+  readonly label: string;
+  readonly noSelect?: boolean;
+  readonly onClick?: MouseEventHandler<HTMLLabelElement>;
+}
+
+const Radio: FunctionComponent<IProps> = (props) => {
+  const {label, noSelect, onClick, className, ...other} = props;
+
+  const rootClasses = [classes["root"]];
+
+  if (noSelect) rootClasses.push(classes["no-select"]);
+  if (className) rootClasses.push(className);
+
+  return <label className={rootClasses.join(" ")} onClick={onClick}>
+    <input type="radio" {...other}></input>
+    <div className={classes["radio"]}>
+      <div className={classes["effect"]}></div>
+    </div>
+    {label && <span className={classes["label"]}>{label}</span>}
+  </label>;
+};
+
+export default memo(Radio);
