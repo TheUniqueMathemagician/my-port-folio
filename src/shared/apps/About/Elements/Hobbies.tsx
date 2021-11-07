d92c703c1514b71dcf3d6bce75cd1841bc2c5f03
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-11-07 19:49:30 +0100

    feat(About): added About app component

---
+++ src/shared/apps/About/Elements/Hobbies.tsx
@@ -0,0 +1,65 @@
+import {FC, memo} from "react";
+import Paper from "../../../ui/Paper";
+import Typography from "../../../ui/Typography";
+import classes from "./Hobbies.module.scss";
+
+interface Props {
+  pid: string;
+}
+
+const Hobbies: FC<Props> = () => {
+  return <div className={classes["root"]}>
+    <Typography variant="h4">Loisirs</Typography>
+    <Typography variant="h5">Code</Typography>
+    <Paper spaced background="paper" className={classes["paper"]}>
+      <img
+        src={require("../../../../assets/images/coding.svg").default}
+        alt="Coding"
+      />
+      <Typography variant="p">
+        Evidemment le code et le développement en font partie. J'ai commencé
+        vers à l'adolescence et j'ai très vite accroché. Bien que la vie et
+        les choix que j'ai fait m'ont amené à étudier l'électromécanique,
+        j'aimerais coupler mes études à ma passion.
+      </Typography>
+    </Paper>
+    <Typography variant="h5">Guitare</Typography>
+    <Paper spaced background="paper" className={classes["paper"]}>
+      <img
+        src={require("../../../../assets/images/guitar.svg").default}
+        alt="Coding"
+      />
+      <Typography variant="p">
+        Je joue de la guitare depuis une bonne décénie et je ne m'en lasse
+        pas. Actuellement je ne suis pas dans un groupe, mais si vous cherchez
+        un guitariste dans la région de Liège et que vous aimez le métal \m/ (
+        PS: Je ne suis pas pour autant sataniste )
+      </Typography>
+    </Paper>
+    <Typography variant="h5">Nature</Typography>
+    <Paper spaced background="paper" className={classes["paper"]}>
+      <img
+        src={require("../../../../assets/images/nature.svg").default}
+        alt="Coding"
+      />
+      <Typography variant="p">
+        J'aime tout ce qui touche à la nature, que ce soit une promenade en
+        forêt ou encore entretenir mon petit potager.
+      </Typography>
+    </Paper>
+    <Typography variant="h5">Technologies</Typography>
+    <Paper spaced background="paper" className={classes["paper"]}>
+      <img
+        src={require("../../../../assets/images/electronics.svg").default}
+        alt="Coding"
+      />
+      <Typography variant="p">
+        Je suis un grand amateur de nouvelles technologies, qu'elles soient
+        domestiques ou industrielles. Il m'arrive de temps à autres de
+        bricoler, que ce soit arduinos, raspberry pi, serveurs domotiques, ...
+      </Typography>
+    </Paper>
+  </div>;
+};
+
+export default memo(Hobbies);
---
+++ src/shared/apps/About/index.tsx
@@ -0,0 +1,122 @@
+import {FC, memo, useCallback, useState} from "react";
+import {MdSend} from "react-icons/md";
+import {useDispatch, useSelector} from "../../../hooks/Store";
+import {runApplication} from "../../../store/slices/Applications";
+import {
+  Applications,
+  WindowInstance
+} from "../../../store/slices/Applications/Types";
+import {EBreakpoints} from "../../../types/EBreakpoints";
+import {EColorScheme} from "../../../types/EColorScheme";
+import Button from "../../ui/input/Button";
+import Tab from "../../ui/Tab";
+import TabPanel from "../../ui/TabPanel";
+import Tabs from "../../ui/Tabs";
+import Hobbies from "./Elements/Hobbies";
+import Intro from "./Elements/Intro";
+import Skills from "./Elements/Skills";
+import classes from "./index.module.scss";
+
+interface Props {
+  pid: string;
+}
+
+enum ETabs {
+  about,
+  skills,
+  hobbies
+}
+
+const Send = memo(MdSend);
+
+const About: FC<Props> = (props) => {
+  const {pid} = props;
+
+  const [panelIndex, setPanelIndex] = useState<number>(0);
+
+  const dispatch = useDispatch();
+
+  const contrast = useSelector((store) => store.theme.colorScheme === EColorScheme.contrast);
+  const resizing = useSelector((store) => store.applications.instances[pid] as WindowInstance).resizing;
+  const small = useSelector((store) => {
+    const instance = store.applications.instances[pid] as WindowInstance;
+    if (instance.breakpoint === EBreakpoints.sm) return true;
+    if (instance.breakpoint === EBreakpoints.xs) return true;
+    return false;
+  });
+
+  const handleContactClick = useCallback(() => {
+    dispatch(runApplication({aid: Applications.Contact, args: {}}));
+  }, [dispatch]);
+
+  const leftBarClasses = [classes["left-bar"]];
+  const rootClasses = [classes["root"]];
+
+  if (contrast) leftBarClasses.push(classes["contrast"]);
+  if (small) rootClasses.push(classes["small"]);
+
+  return <div className={rootClasses.join(" ")}>
+    <div className={leftBarClasses.join(" ")}>
+      <Tabs
+        direction={small ? "top" : "right"}
+        defaultValue={0}
+        onChange={(v: number) => setPanelIndex(v)}
+        shouldRefresh={resizing}
+      >
+        <Tab
+          active={panelIndex === ETabs.about}
+          label="Profil"
+          value={ETabs.about}
+        />
+        <Tab
+          active={panelIndex === ETabs.skills}
+          label="Compétences"
+          value={ETabs.skills}
+        />
+        <Tab
+          label="Loisirs"
+          active={panelIndex === ETabs.hobbies}
+          value={ETabs.hobbies}
+        />
+      </Tabs>
+      {!small && (
+        <Button
+          focusable
+          fullWidth
+          startIcon
+          color="primary"
+          onClick={handleContactClick}
+        >
+          <Send></Send>
+          <span>Contacter</span>
+        </Button>
+      )}
+    </div>
+    <TabPanel
+      className={classes["tab-panel"]}
+      index={ETabs.about}
+      value={panelIndex}
+      spaced
+    >
+      <Intro pid={pid}></Intro>
+    </TabPanel>
+    <TabPanel
+      className={classes["tab-panel"]}
+      index={ETabs.skills}
+      value={panelIndex}
+      spaced
+    >
+      <Skills pid={pid}></Skills>
+    </TabPanel>
+    <TabPanel
+      className={classes["tab-panel"]}
+      index={ETabs.hobbies}
+      value={panelIndex}
+      spaced
+    >
+      <Hobbies pid={pid}></Hobbies>
+    </TabPanel>
+  </div>;
+};
+
+export default memo(About);
