c104240ce35adf58446e2cc0c9c01e46f07692b9
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-11-07 17:00:41 +0100

    feat(TaskBarTimeDate): added TaskBarTimeDate component

--- src/shared/os/TaskBarTimeDate.tsx
+++ src/shared/os/TaskBarTimeDate.tsx
@@ -1,16 +1,12 @@
+import {useEffect, useState} from "react";
 import classes from "./TaskBarTimeDate.module.scss";
-import { useEffect, useState } from "react";
 
 const TaskBarTimeDate = () => {
   const [date, setDate] = useState<number>(Date.now());
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setDate(Date.now());
-    }, 1000);
-    return () => {
-      clearInterval(interval);
-    };
+    const interval = setInterval(() => {setDate(Date.now());}, 1000);
+    return () => {clearInterval(interval);};
   }, []);
 
   const _time = new Date(date).toLocaleTimeString().slice(0, -3);
@@ -20,12 +16,11 @@ const TaskBarTimeDate = () => {
     .split("/")
     .map((x) => x.match(/\d{2}$/))
     .join("-");
-  return (
-    <div className={classes["root"]}>
-      <div>{_time}</div>
-      <div>{_date}</div>
-    </div>
-  );
+
+  return <div className={classes["root"]}>
+    <div>{_time}</div>
+    <div>{_date}</div>
+  </div>;
 };
 
 export default TaskBarTimeDate;
