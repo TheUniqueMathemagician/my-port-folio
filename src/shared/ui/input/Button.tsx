554726b2880e3348a4b9c8d3c39666f4a98bf595
Mathemagician <53182934+TheUniqueMathemagician@users.noreply.github.com>
2021-11-11 15:31:16 +0100

    fix(Button): fixed breaking changes

--- src/shared/ui/input/Button.tsx
+++ src/shared/ui/input/Button.tsx
@@ -1,4 +1,5 @@
-import React, {AnchorHTMLAttributes, ButtonHTMLAttributes, DetailedHTMLProps, forwardRef, memo, PropsWithChildren, RefObject, useCallback, useEffect, useRef, useState} from "react";
+import Link from "next/link";
+import React, {AnchorHTMLAttributes, ButtonHTMLAttributes, DetailedHTMLProps, forwardRef, PropsWithChildren, RefObject, useCallback, useEffect, useRef} from "react";
 import contrastColor from "../../../functions/contrastColor";
 import {useSelector} from "../../../hooks/Store";
 import {TColor} from "../../../types/TColor";
@@ -44,61 +45,57 @@ interface PropsButtonB extends HTMLProps {
 }
 
 const Button = forwardRef<HTMLButtonElement, PropsWithChildren<PropsButtonA | PropsButtonB>>((props, parentRef) => {
-  const {ripple, readOnly, color, fullWidth, size, variant, startIcon, endIcon, align, contrast, outlined, isIcon, className, loading, children, ...rest} = props;
+  const {ripple, readOnly, color, fullWidth, size, variant, startIcon, endIcon, align, contrast, outlined, isIcon, className, loading, children, onClick, ...rest} = props;
 
-  const innerRef = useRef<HTMLButtonElement>(null);
+  const innerRef = useRef<HTMLElement>(null);
   const ref = (parentRef as RefObject<HTMLButtonElement>) ?? innerRef;
-
-  const [mouseHasBeenDown, setMouseHasBeenDown] = useState<boolean>(false);
 
   const palette = useSelector((store) => store.theme.palette);
   const colorScheme = useSelector((store) => store.theme.colorScheme);
 
   const handleKeyPress = useCallback((e: React.KeyboardEvent<HTMLElement>) => {
-    if (e.code === "Space" && ref && ripple && !readOnly) {
-      const button = ref.current;
-      if (button) {
-        const x = button.clientWidth / 2;
-        const y = button.clientHeight / 2;
-        const ripples = document.createElement("span");
-        ripples.classList.add(classes["ripple"]);
-        ripples.style.left = `${ x }px`;
-        ripples.style.top = `${ y }px`;
-        button.appendChild(ripples);
-        setTimeout(() => {ripples.remove();}, 666);
-      }
-    }
-  }, [ref, ripple, readOnly]);
+    if (e.code !== "Space" || !ripple || readOnly || !ref.current) return;
 
-  const handleMouseDown = useCallback(() => {
-    setMouseHasBeenDown(true);
-  }, []);
+    const button = ref.current;
 
-  const handleMouseUp = useCallback((e: React.MouseEvent) => {
-    if (mouseHasBeenDown && ref && ripple && !readOnly) {
-      const button = ref.current;
-      if (button) {
-        const BC = button.getBoundingClientRect();
-        const x = e.clientX - BC.x;
-        const y = e.clientY - BC.y;
-        const ripple = document.createElement("span");
-        ripple.classList.add(classes["ripple"]);
-        ripple.style.left = `${ x }px`;
-        ripple.style.top = `${ y }px`;
-        button.prepend(ripple);
-        setTimeout(() => {ripple.remove();}, 666);
-        setMouseHasBeenDown(false);
-      }
-    }
-  }, [mouseHasBeenDown, ref, ripple, readOnly]);
+    const x = button.clientWidth / 2;
+    const y = button.clientHeight / 2;
+    const ripples = document.createElement("span");
+
+    ripples.classList.add(classes["ripple"]);
+    ripples.style.left = `${ x }px`;
+    ripples.style.top = `${ y }px`;
+
+    button.appendChild(ripples);
+
+    setTimeout(() => {ripples.remove();}, 666);
+  }, [ripple, readOnly]);
+
+  const handleClick = useCallback((e: React.MouseEvent) => {
+    if (!ripple || readOnly || !ref.current) return;
+
+    const BC = ref.current.getBoundingClientRect();
+    const x = e.clientX - BC.x;
+    const y = e.clientY - BC.y;
+    const element = document.createElement("span");
+
+    element.classList.add(classes["ripple"]);
+    element.style.left = `${ x }px`;
+    element.style.top = `${ y }px`;
+
+    ref.current.prepend(element);
+
+    setTimeout(() => {element.remove();}, 666);
+  }, [ripple, readOnly]);
 
   useEffect(() => {
+    if (!ref.current) return;
+
     const button = ref.current;
-    if (button) {
-      const backgroundColor = palette[color ?? "background"][colorScheme];
-      button.style.setProperty("--text-color", contrastColor(backgroundColor));
-    }
-  }, [ref, colorScheme, palette, color]);
+
+    const backgroundColor = palette[color ?? "background"][colorScheme];
+    button.style.setProperty("--text-color", contrastColor(backgroundColor));
+  }, [colorScheme, palette, color]);
 
   const rootClasses = [classes["root"]];
 
@@ -115,16 +112,18 @@ const Button = forwardRef<HTMLButtonElement, PropsWithChildren<PropsButtonA | Pr
   if (className) rootClasses.push(className);
   if (loading) rootClasses.push(classes["loading"]);
 
-  if (props.to) return <a href={props.to} className={rootClasses.join(" ")} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} onKeyPress={handleKeyPress} {...rest}>
-    <div className={classes["content"]}>{children}</div>
-    <div className={classes["loader"]}>
-      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="50" cy="50" r="45" />
-      </svg>
-    </div>
-  </a>;
+  if (props.to) return <Link href={props.to}>
+    <a className={rootClasses.join(" ")} {...rest}>
+      <div className={classes["content"]}>{children}</div>
+      <div className={classes["loader"]}>
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="50" cy="50" r="45" />
+        </svg>
+      </div>
+    </a>
+  </Link>;
 
-  return <button className={rootClasses.join(" ")} onMouseUp={handleMouseUp} onMouseDown={handleMouseDown} onKeyPress={handleKeyPress} {...rest}>
+  return <button className={rootClasses.join(" ")} onClick={handleClick} onKeyPress={handleKeyPress} ref={ref} {...rest}>
     <div className={classes["content"]}>{children}</div>
     <div className={classes["loader"]}>
       <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
@@ -134,4 +133,4 @@ const Button = forwardRef<HTMLButtonElement, PropsWithChildren<PropsButtonA | Pr
   </button>;
 });
 
-export default memo(Button);
+export default Button;
