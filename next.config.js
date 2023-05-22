073baec35c7d96ff7b64716dedf5e57054a4abd0
Yannick Tamburrini <53182934+TheUniqueMathemagician@users.noreply.github.com>
2023-05-23 01:29:16 +0200

    chore(next): remove old headers

--- next.config.js
+++ next.config.js
@@ -1,31 +1,7 @@
 /** @type {import('next').NextConfig} */
-
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com"]
-  },
-  webpack5: true,
-  headers: [
-    {
-      key: 'X-DNS-Prefetch-Control',
-      value: 'on'
-    },
-    {
-      key: 'X-XSS-Protection',
-      value: '1; mode=block'
-    },
-    {
-      key: 'Content-Security-Policy',
-      value: 'default-src \'self\''
-    },
-    {
-      key: 'X-Content-Type-Options',
-      value: 'nosniff'
-    },
-    {
-      key: 'X-Frame-Options',
-      value: 'sameorigin'
-    }
-  ]
-};
+	reactStrictMode: true,
+	images: {
+		domains: ["images.unsplash.com"]
+	},
+}
