c0b5676ce843c07637b72f660e0bf29c3540a11b
Yannick Tamburrini <53182934+TheUniqueMathemagician@users.noreply.github.com>
2023-06-03 23:23:54 +0200

    refactor(utils): removed magic numbers

--- utils/contrastColor.ts
+++ utils/contrastColor.ts
@@ -1,11 +1,26 @@
-const contrastColor = (hex: string) => {
-	hex = hex.substring(1)
+const RED_PERCEPTION_RATIO = 0.3
+const GREEN_PERCEPTION_RATIO = 0.6
+const BLUE_PERCEPTION_RATIO = 0.1
 
-	const r = parseInt(hex.slice(0, 2), 16)
-	const g = parseInt(hex.slice(2, 4), 16)
-	const b = parseInt(hex.slice(4, 6), 16)
+const PERCEPTION_THRESHOLD = 144
 
-	return r * 0.3 + g * 0.6 + b * 0.1 > 144 ? "#000000" : "#ffffff"
+const RED_START_INDEX = 0
+const RED_END_INDEX = 2
+
+const GREEN_START_INDEX = RED_END_INDEX
+const GREEN_END_INDEX = 4
+
+const BLUE_START_INDEX = GREEN_END_INDEX
+const BLUE_END_INDEX = 6
+
+const contrastColor = (hexDigits: string) => {
+	hexDigits = hexDigits.substring(1)
+
+	const red = parseInt(hexDigits.slice(RED_START_INDEX, RED_END_INDEX), 16)
+	const green = parseInt(hexDigits.slice(GREEN_START_INDEX, GREEN_END_INDEX), 16)
+	const blue = parseInt(hexDigits.slice(BLUE_START_INDEX, BLUE_END_INDEX), 16)
+
+	return red * RED_PERCEPTION_RATIO + green * GREEN_PERCEPTION_RATIO + blue * BLUE_PERCEPTION_RATIO > PERCEPTION_THRESHOLD ? "#000000" : "#ffffff"
 }
 
 export default contrastColor
